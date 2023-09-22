4c16850668f6e5e15b1a10e45ddc3963f2d7d27a
JuChan Jeong <jjc6252498@gmail.com>
2023-09-22 11:41:14 +0900

    인벤토리 아이템 개수와 stat 텍스트 아이템 적용 표시 구현

--- Assets/Scripts/UI/StatusUI.cs
+++ Assets/Scripts/UI/StatusUI.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,6 +20,8 @@ public class StatusUI : MonoBehaviour
     [Header("Main Choice UI")]
     [SerializeField] private GameObject mainChoicePanel;
 
+    private CharacterData characterData;
+
     private void OnEnable()
     {
         statusBackButton.onClick.AddListener(OnClickBackButton);
@@ -26,6 +29,8 @@ public class StatusUI : MonoBehaviour
 
     private void Start()
     {
+        characterData = CharacterStat.instance.characterData;
+        UpdateUI(characterData);
         gameObject.SetActive(false);
     }
 
@@ -40,8 +45,28 @@ public class StatusUI : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    public void UpdateUI()
+    public void UpdateUI(ItemData itemData)
     {
+        attackText.text = GetStatText(characterData.attack, itemData.attack);
+        shieldText.text = GetStatText(characterData.defense, itemData.defense);
+        healthText.text = GetStatText(characterData.health, itemData.health);
+        criticalText.text = GetStatText(characterData.critical, itemData.critical);
+    }
 
+    private string GetStatText(float characterStat, float itemStat)
+    {
+        string statText = itemStat == 0f ?
+            $"{characterStat + itemStat}":
+            $"{characterStat + itemStat} ({characterStat} + <color=green>{itemStat}</color>)";
+
+        return statText;
+    }
+
+    public void UpdateUI(CharacterData characterData)
+    {
+        attackText.text = characterData.attack.ToString();
+        shieldText.text = characterData.defense.ToString();
+        healthText.text = characterData.health.ToString();
+        criticalText.text = characterData.critical.ToString();
     }
 }
